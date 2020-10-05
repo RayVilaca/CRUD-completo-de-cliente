31992777b89a6ad61556098b4c09164f72966c46
Rayssa Vilaca <cecilia_vilaca@outlook.com>
2020-10-05 03:19:59 -0300

    INSERT, UPDATE, DELETE, PAGINATION

--- backend/src/main/java/com/devsuperior/trabalhofinalcap1/entities/Client.java
+++ backend/src/main/java/com/devsuperior/trabalhofinalcap1/entities/Client.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +28,13 @@ public class Client implements Serializable {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
 	
 	public Client() {
 		
@@ -86,6 +95,16 @@ public class Client implements Serializable {
 
 	public void setChildren(Integer children) {
 		this.children = children;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,14 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria', '2452245', 150.5, NOW(), 5);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marta', '2452775', 188., NOW(), 0);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marcos', '2452247', 9.25, NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Kurenai', '2452245', 150.5, NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Kakashi', '2452775', 188., NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Shikamaro', '2452247', 9.25, NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sakura', '2451445', 200., NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sasuke', '2452175', 188., NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Naruto', '2452047', 900.25, NOW(), 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Konan', '24522750', 15.5, NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jiraya', '2452771', 1808., NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Itachi', '2452287', 99.25, NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Hinata', '2452047', 900.25, NOW(), 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Rock Lee', '24522750', 15.5, NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Tsunade', '2452771', 1808., NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Gaara', '2452287', 99.25, NOW(), 0);
+
